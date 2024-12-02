ecbfc2ca8a74ac144cc94e8af47b6840c3e9af80
Gabriel Seroiska Guidini <gabriel.guidini@dasa.com.br>
2024-12-02 13:07:00 -0300

    adding returning response

--- check-app/src/main/java/br/com/check/app/controller/UnitController.java
+++ check-app/src/main/java/br/com/check/app/controller/UnitController.java
@@ -4,8 +4,10 @@ import br.com.check.app.dto.UnitExamDto;
 import br.com.check.app.entity.Unit;
 import br.com.check.app.service.UnitExamService;
 import br.com.check.app.service.UnitService;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,26 +27,30 @@ public class UnitController {
   private final UnitExamService unitExamService;
 
   @PostMapping
+  @ResponseStatus(HttpStatus.OK)
   public void unitCreation(@RequestParam String name, @RequestParam String region, @RequestParam String specialty, @RequestParam String services) {
 
     this.unitService.createUnit(name, region, specialty, services);
-
   }
 
   @GetMapping("/{unitId}")
+  @ResponseStatus(HttpStatus.OK)
+  @Transactional
   public Unit findUnitById(@PathVariable Long unitId) {
 
     return this.unitService.findUnitByUnitId(unitId);
   }
 
   @GetMapping("/{unitId}/unitExams")
+  @ResponseStatus(HttpStatus.OK)
   public List<UnitExamDto> findUnitExamsByUnitId(@PathVariable Long unitId) {
 
     return unitExamService.findAllExamsByUnitId(unitId);
   }
 
-
   @PatchMapping("/{unitId}")
+  @ResponseStatus(HttpStatus.OK)
+  @Transactional
   public void addExamIntoUnit(@PathVariable Long unitId,
                               @RequestBody List<UnitExamDto> unitExamDto) {
 
