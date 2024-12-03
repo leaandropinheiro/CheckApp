d7258a8da5235d58b822e8c17ac4c6c0431dd3ae
Gabriel Seroiska Guidini <gabriel.guidini@dasa.com.br>
2024-12-03 14:54:44 -0300

    adjust

--- check-app/src/main/java/br/com/check/app/utils/UnitExamUtils.java
+++ check-app/src/main/java/br/com/check/app/utils/UnitExamUtils.java
@@ -24,6 +24,7 @@ public class UnitExamUtils {
         .examName(unit.getExamName())
         .examCode(unit.getExamCode())
         .unitId(unit.getUnitId())
+        .examValue(unit.getExamValue())
         .build();
   }
 }
