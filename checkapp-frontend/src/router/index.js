89076587b53e93639190533880b98800aff3d70f
Leandro Pinheiro <leandropinheirorosa1@hotmail.com>
2024-12-04 12:40:29 -0300

    fix router meta title

--- checkapp-frontend/src/router/index.js
+++ checkapp-frontend/src/router/index.js
@@ -1,13 +1,16 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 import store from "@/store";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
 router.beforeEach((to, from, next) => {
+
+  document.title = `CheckApp - ${to.meta.title}`;
+
   if (to.meta.requiresAuth && !store.getters["auth/isAuthenticated"]) {
     next("/login");
   } else {
